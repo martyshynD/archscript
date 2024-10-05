c0753b70c25f3366b5554c70f055e8f5c0eb2f0b
martyshynD <132333148+martyshynD@users.noreply.github.com>
2024-10-05 13:29:19 +0300

    Update archscript2.sh

--- archscript2.sh
+++ archscript2.sh
@@ -1,3 +1,29 @@
+
+# timezone 
+timedatectl set-timezone Europe/Kyiv
+
+# set to system clock 
+hwclock --systohc 
+
+# locales
+echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen 
+
+# generate locale 
+locale-gen
+
+# locales 2 
+echo 'LANG=en_US.UTF-8' >> /etc/locale.conf 
+
+# Root password
+passwd
+
+# Boot Loader
+pacman -S grub networkmanager network-manager-applet dialog wireless_tools wpa_supplicant os-prober mtools dosfstools base-devel linux-headers -y 
+
+# Install grub 
+grub-install --target=i386-pc /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg 
+
 # Activate Internet 
 systemctl start NetworkManager 
 
